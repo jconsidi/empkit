2a7a956ea22b24b9a932f21df01ff997e6fd9b5a
Jeffrey Considine <jconsidi@gmail.com>
2025-06-07 12:03:11 -0400

    + refactored tests with check_prediction

--- tests/test_1d.py
+++ tests/test_1d.py
@@ -5,6 +5,10 @@ import unittest
 from empkit import EnsembleOfManyProjections
 
 class EMP1dTestCase(unittest.TestCase):
+    def check_prediction(self, m, X, y):
+        with self.subTest(X=X):
+            self.check_value(m.predict([X]), y)
+
     def check_value(self, v, v_expected):
         self.assertEqual(v.size, 1)
         self.assertAlmostEqual(v.reshape(()), v_expected)
@@ -16,20 +20,20 @@ class EMP1dTestCase(unittest.TestCase):
                 m.fit([[0], [1]], [0, 1])
 
                 # training data points
-                self.assertEqual(m.predict([[0.00]]), [0.00])
-                self.assertEqual(m.predict([[1.00]]), [1.00])
+                self.check_prediction(m, [0.00], 0.00)
+                self.check_prediction(m, [1.00], 1.00)
 
                 # interpolation
-                self.check_value(m.predict([[0.25]]), 0.25)
-                self.check_value(m.predict([[0.50]]), 0.50)
-                self.check_value(m.predict([[0.75]]), 0.75)
+                self.check_prediction(m, [0.25], 0.25)
+                self.check_prediction(m, [0.50], 0.50)
+                self.check_prediction(m, [0.75], 0.75)
 
                 # extrapolation
-                self.check_value(m.predict([[-1.00]]), 0.00)
-                self.check_value(m.predict([[-0.75]]), 0.00)
-                self.check_value(m.predict([[-0.50]]), 0.00)
-                self.check_value(m.predict([[-0.25]]), 0.00)
-                self.check_value(m.predict([[+1.25]]), 1.00)
-                self.check_value(m.predict([[+1.50]]), 1.00)
-                self.check_value(m.predict([[+1.75]]), 1.00)
-                self.check_value(m.predict([[+2.00]]), 1.00)
+                self.check_prediction(m, [-1.00], 0.00)
+                self.check_prediction(m, [-0.75], 0.00)
+                self.check_prediction(m, [-0.50], 0.00)
+                self.check_prediction(m, [-0.25], 0.00)
+                self.check_prediction(m, [+1.25], 1.00)
+                self.check_prediction(m, [+1.50], 1.00)
+                self.check_prediction(m, [+1.75], 1.00)
+                self.check_prediction(m, [+2.00], 1.00)
